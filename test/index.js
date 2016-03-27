61d34686d4db4324ec29778e24f8cb485ed92d0b
Dave Hudson <dave@applification.net>
2016-03-27 20:55:15 +0100

    Tests

--- test/index.js
+++ test/index.js
@@ -32,3 +32,36 @@ test('sign in', tt => {
       tt.end()
     })
 })
+
+// IF I do this, it's going to sign up a new user everytime. So ideally I want to delete this user once signed up
+// Suspect this means I need a delete account option
+test('sign up', tt => {
+  request(app)
+    .post('/signup')
+    .send('{ "email": "dave12@me.com", "password": "123" }')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+      tt.error(err, 'No error')
+      // tt.comment(res.body.token)
+      tt.ok(res.body.token, 'Token node present')
+      tt.end()
+    })
+})
+
+// Sign Up: User Already Exists
+test('Sign up: User already exists', tt => {
+  request(app)
+    .post('/signup')
+    .send('{ "email": "dave12@me.com", "password": "123" }')
+    .expect('Content-Type', /json/)
+    .expect(422)
+    .end((err, res) => {
+      const json = {
+        error: 'Email is in use'
+      }
+      tt.error(err, 'No error')
+      tt.same(res.body, json, 'JSON: Email in use returned')
+      tt.end()
+    })
+})
